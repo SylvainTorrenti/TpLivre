ffa41d9b9ab6c10ca5dc84b31a10418a4c3e7900
SylvainTorrenti <torrsyl@gmail.com>
2023-01-12 20:27:23 +0100

    Ajoutez des fichiers projet.

---
+++ TpLivre/Book.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TpLivre
+{
+    internal class Book
+    {
+        #region Attribute
+        private string _title;
+        private string _autor;
+        private double _price;
+        #endregion
+        #region Constructor
+        /// <summary>
+        /// Constructor 
+        /// </summary>
+        /// <param name="title">Titre</param>
+        /// <param name="autor">Auteur</param>
+        /// <param name="price">Prix</param>
+        public Book(string title, string autor, double price)
+        {
+            _title = title;
+            _autor = autor;
+            _price = price;
+        }
+        #endregion
+        #region Get & Set Title
+        public string Title { get => _title; set => _title = value; }
+        #endregion
+        #region Get & Set Autor
+        public string Autor { get => _autor; set => _autor = value; }
+        #endregion
+        #region Get & Set Price
+        public double Price { get => _price; set => _price = value; }
+        #endregion
+        #region Method
+        /// <summary>
+        /// Method to display book
+        /// </summary>
+        public void Print()
+        {
+            Console.WriteLine($"Titre : {Title}");
+            Console.WriteLine($"Auteur : {Autor}");
+            Console.WriteLine($"Prix : {Price} euros");
+            Console.WriteLine();
+        } 
+        #endregion
+    }
+}
---
+++ TpLivre/Program.cs
@@ -0,0 +1,10 @@
+﻿using TpLivre;
+
+#region Book creation
+Book Book1 = new Book("Le Pacte des MarchOmbres", "Pierre Bottero", 17.58);
+Book Book2 = new Book("Ellana l'envol", "Pierre Bottero", 13.36);
+#endregion
+#region Display book
+Book1.Print();
+Book2.Print(); 
+#endregion
